911a21ee7f30a17a3551718d48be03a6e5ffeb95
jio <pennicillium@daum.net>
2024-06-30 23:37:17 +0900

    [modify] Svg 컴포넌트 로고 속성 추가 #9

--- src/components/atoms/Svg/Svg.tsx
+++ src/components/atoms/Svg/Svg.tsx
@@ -1,14 +1,38 @@
 interface Svg {
-  id: string;
-  color?: string;
+  id?: string;
+  color?: "black" | "white";
   size?: number;
+  logo?: boolean;
+  mobile?: boolean;
 }
 
 // import Sprite from "@/assets/sprite.svg";
-export default function Svg({ id, color, size = 24 }: Svg) {
-  return (
-    <svg style={{ color }} width={size} height={size}>
-      <use href={`sprite.svg#${id}`} />
-    </svg>
-  );
+export default function Svg({
+  id,
+  color = "black",
+  size = 24,
+  logo,
+  mobile,
+}: Svg) {
+  if (!logo) {
+    return (
+      <svg style={{ color }} width={size} height={size}>
+        <use href={`sprite.svg#${id}`} />
+      </svg>
+    );
+  } else {
+    if (mobile) {
+      return (
+        <svg style={{ color }}>
+          <use href={`logo.svg#mobile-${color}`} />
+        </svg>
+      );
+    } else {
+      return (
+        <svg style={{ color }}>
+          <use href={`logo.svg#web-${color}`} />
+        </svg>
+      );
+    }
+  }
 }
