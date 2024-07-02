e7bc3d489013b1fffeef495ff2663cae1cab58b8
jio <pennicillium@daum.net>
2024-07-02 18:58:52 +0900

    [modify] SearhInput component 스타일 코드 삭제 #14

--- src/components/atoms/SearchInput/SearchInput.tsx
+++ src/components/atoms/SearchInput/SearchInput.tsx
@@ -1,11 +1,14 @@
 import { Svg } from "@/components/atoms";
-export default function SearchInput() {
+type TSearchInput = {
+  className?: string;
+};
+export default function SearchInput({ className }: TSearchInput) {
   return (
-    <div className="relative flex w-[50%] bg-accent-red">
+    <div className={`relative h-[80px] w-[50%] ${className}`}>
       <input
         type="text"
         placeholder="상품명 또는 #태그명으로 검색하세요"
-        className="placholder:text-gray-300 h-[80px] w-full border-b-[7px] border-black text-h-1-regular"
+        className="placholder:text-gray-300 h-[80px] w-full border-b-[7px] border-black bg-transparent text-h-1-regular"
       />
       <div className="absolute bottom-[25%] right-0">
         <Svg id="search" size={50} />
