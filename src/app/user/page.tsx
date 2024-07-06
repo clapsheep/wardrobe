87ba29371b9a2ce9a2678ef47e4fb02014839126
clapsheep <sktndid1203@naver.com>
2024-07-07 00:23:36 +0900

    [feat] Tag 컴포넌트 제작 - TagInput 제작 - TagButton 제작

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,3 +1,21 @@
+import { TagInput } from "@/components/atoms";
+
 export default async function Page() {
-  return <div></div>;
+  return (
+    <div className="flex gap-4">
+      <TagInput id="name" name="name" type="checkbox">
+        체크박스
+      </TagInput>
+      <div className="flex flex-col gap-4">
+        <div className="flex">
+          <TagInput type="radio" name="season" id="spring">
+            봄
+          </TagInput>
+          <TagInput type="radio" name="season" id="authmn">
+            가을
+          </TagInput>
+        </div>
+      </div>
+    </div>
+  );
 }
