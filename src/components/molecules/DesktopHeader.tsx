c6816f4519cef74b7203ad3d3092be5e222af1e1
jio <pennicillium@daum.net>
2024-07-07 23:05:43 +0900

    [chore] 컴포넌트 분리 #14

---
+++ src/components/molecules/DesktopHeader.tsx
@@ -0,0 +1,99 @@
+"use client";
+import Link from "next/link";
+import { Svg } from "@/components/atoms";
+import { useState } from "react";
+import { usePathname } from "next/navigation";
+import { MobileHamburger, SearchModal } from "@/components/molecules";
+
+type THeader = {
+  isScrolled: boolean;
+};
+
+const userMenuItems = () => {
+  // 로그인여부 전역상태관리 지정후 수정 필요
+  const isLogin = false;
+  return [
+    { href: "/profile", iconId: "person", text: "Profile" },
+    { href: "/bookmark", iconId: "bookmark", text: "Bookmark" },
+    isLogin
+      ? { href: "/logout", iconId: "log-out", text: "Logout" }
+      : { href: "/login", iconId: "log-in", text: "Login" },
+  ];
+};
+
+const navItems = [
+  { href: "/dressroom", text: "Dressroom" },
+  { href: "/style", text: "Style" },
+];
+
+export default function DesktopHeader({ isScrolled }: THeader) {
+  const pathname = usePathname();
+  const [isSearchState, setIsSearchState] = useState<boolean>(false);
+  return (
+    <header
+      className={`w-full px-[83px] font-sans ${
+        isScrolled ? "mt-[73px]" : "pt-[52px]"
+      }`}
+    >
+      {!isScrolled && (
+        <ul className="font-gray-800 flex justify-end gap-4 text-center text-b-0-regular">
+          {userMenuItems().map((item) => (
+            <li key={item.iconId}>
+              <Link href={item.href} className={`flex items-center gap-1`}>
+                <Svg id={item.iconId} size={18} />
+                {item.text}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+      <nav>
+        <ul className="flex grid grid-cols-7 grid-rows-1 items-center text-h-2-bold">
+          <div className="col-span-2 col-start-1 flex gap-[42px]">
+            {navItems.map((item) => (
+              <li
+                className={`${pathname === `${item.href}` ? "underline decoration-8 underline-offset-8" : ""}`}
+                key={item.text}
+              >
+                <Link href={item.href}>{item.text}</Link>
+              </li>
+            ))}
+          </div>
+          <li className="col-span-1 col-start-4 mx-auto max-w-full">
+            <Link className="w-[265px] max-w-full" href="/">
+              <Svg mobile logo size={265} />
+            </Link>
+          </li>
+          <ul
+            className={`${
+              isScrolled
+                ? "col-span-2 col-start-6 flex gap-[34px] justify-self-center"
+                : "hidden"
+            }`}
+          >
+            {userMenuItems().map((item) => (
+              <li key={item.iconId}>
+                <Link href={item.href}>
+                  <Svg id={item.iconId} size={28} />
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <li className="col-start-8 justify-self-end">
+            <button onClick={() => setIsSearchState(true)}>
+              <Svg id="search" size={isScrolled ? 28 : 38} color="black" />
+            </button>
+          </li>
+        </ul>
+      </nav>
+      {isSearchState ? (
+        <SearchModal
+          closeFn={() => setIsSearchState(false)}
+          open={isSearchState}
+        />
+      ) : (
+        ""
+      )}
+    </header>
+  );
+}
--- src/components/molecules/MobileHamburger.tsx
+++ src/components/molecules/MobileHamburger.tsx
@@ -1,66 +1,71 @@
-import Image from "next/image";
 import Link from "next/link";
 import { Svg } from "@/components/atoms";
+
 interface MenuType {
-  children: string;
   href: string;
+  text: string;
 }
 const sitemap = {
-  main: {
-    Home: "/",
-    Dressroom: "/",
-    Style: "/",
-    Profile: "/",
+  main: () => {
+    return [
+      { text: "Home", href: "/" },
+      { text: "Dressroom", href: "/dressroom" },
+      { text: "Style", href: "/style" },
+      { text: "Profile", href: "/profile" },
+    ];
   },
-  sub: {
-    Login: "/",
-    Bookmark: "/",
-    Search: "/",
+  sub: () => {
+    const isLogin = true;
+    return [
+      { text: "Bookmark", href: "/bookmark" },
+      { text: "Search", href: "/" },
+      isLogin
+        ? { text: "logout", href: "/logout" }
+        : { text: "login", href: "/login" },
+    ];
   },
 };
 
-function MobileSiteMapItem({ children }: MenuType) {
+function MobileSiteMapItem({ text, href }: MenuType) {
   return (
     <li>
       <Link
         className="flex justify-between hover:text-h-3-semibold hover:underline hover:decoration-white"
-        href=""
+        href={href}
       >
-        {children}
+        {text}
         <Svg id="arrow-right" color="white" size={28} />
       </Link>
     </li>
   );
 }
+interface TMobileHamburger {
+  closeFn: () => void;
+}
 
-export default function MobileHamburger() {
+export default function MobileHamburger({ closeFn }: TMobileHamburger) {
   return (
     <div className="flex h-svh flex-col gap-[106px] bg-gray-900 p-[20px] font-sans">
       <div className="flex justify-between">
-        <Link href={sitemap["main"]["Home"]}>
-          <Svg logo mobile color="white" size={127} />
-        </Link>
-        <button>
+        <Svg logo mobile color="white" size={127} />
+        <button onClick={closeFn}>
           <Svg id="cancel" color="white" size={28} />
         </button>
       </div>
       <nav>
         <ul className="flex flex-col gap-[64px] text-h-3-regular text-gray-200">
-          {Object.entries(sitemap["main"]).map(([title, link]) => (
-            <MobileSiteMapItem key={title} href={link}>
-              {title}
-            </MobileSiteMapItem>
+          {sitemap["main"]().map(({ text, href }) => (
+            <MobileSiteMapItem key={text} text={text} href={href} />
           ))}
         </ul>
       </nav>
       <ul className="mt-auto flex justify-between text-b-0-semibold text-gray-300">
-        {Object.entries(sitemap["sub"]).map(([title, link]) => (
+        {sitemap["sub"]().map(({ text, href }) => (
           <li
             className="hover:text-b-0-bold hover:text-white hover:underline hover:decoration-white"
-            key={title}
+            key={text}
           >
-            <Link href={link} />
-            {title}
+            <Link href={href}>{text}</Link>
           </li>
         ))}
       </ul>
