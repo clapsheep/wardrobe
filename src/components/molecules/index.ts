8759eb02beba8b7442ccc461d4e576958943cdfa
clapsheep <sktndid1203@naver.com>
2024-07-08 02:36:56 +0900

    [feat] Main 페이지 캐러셀 구현 #30

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -2,3 +2,4 @@ export { default as Header } from "./Header/Header";
 export { default as Footer } from "./Footer/Footer";
 export { default as SearchModal } from "./SearchModal/SearchModal";
 export { default as MobileHamburger } from "./MobileHamburger/MobileHamburger";
+export { default as MainCarousel } from "./MainCarousel";
