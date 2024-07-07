adeafac06248047ef2960ad22e41054783717a44
jio <pennicillium@daum.net>
2024-07-07 23:05:43 +0900

    [chore] 컴포넌트 분리 #14

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -1,4 +1,5 @@
 export { default as Header } from "./Header";
-export { default as Footer } from "./Footer";
 export { default as SearchModal } from "./SearchModal";
+export { default as MobileHeader } from "./MobileHeader";
+export { default as DesktopHeader } from "./DesktopHeader";
 export { default as MobileHamburger } from "./MobileHamburger";
