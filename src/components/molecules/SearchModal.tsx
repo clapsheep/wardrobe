7f1ad28a1878b46e597dce2940d95f48e7f8e0fe
jio <pennicillium@daum.net>
2024-07-08 18:27:02 +0900

    [chore] 컴포넌트 분리 #14

--- src/components/molecules/SearchModal.tsx
+++ src/components/molecules/SearchModal.tsx
@@ -1,7 +1,19 @@
 "use client";
-import { SearchInput, Svg } from "@/components/atoms";
 import { useState, useEffect } from "react";
-function RecentSearchKeyword() {
+import { SearchInput, Svg } from "@/components/atoms";
+import { RecentSearchKeyword } from "@/components/molecules";
+
+interface TSearchModal {
+  className?: string;
+  open: boolean;
+  closeFn: () => void;
+}
+
+export default function SearchModal({
+  closeFn,
+  className,
+  open,
+}: TSearchModal) {
   const [recentSearchList, setRecentSearchList] = useState<string[]>([]);
   // 전체, 아이템 삭제 함수
   const handleStorage = {
@@ -15,6 +27,7 @@ function RecentSearchKeyword() {
       localStorage.setItem("searchKeyword", JSON.stringify(updatedList));
     },
   };
+
   useEffect(() => {
     try {
       const searchLists = localStorage.getItem("searchKeyword");
@@ -23,57 +36,24 @@ function RecentSearchKeyword() {
       setRecentSearchList([]);
     }
   }, []);
-
-  return (
-    <>
-      <SearchInput setList={setRecentSearchList} />
-      <div className="flex w-[50%] flex-col gap-[24px] py-3 mobile:w-full">
-        <div className="flex justify-between">
-          <h2 className="text-h-1-semibold mobile:text-h-3-semibold">
-            최근 검색어
-          </h2>
-          <button
-            onClick={handleStorage.allClear}
-            className="text-h-4-semibold text-gray-400 mobile:text-h-5-semibold"
-          >
-            전체 삭제
-          </button>
-        </div>
-        <ul className="flex flex-col gap-[24px] text-h-4-regular text-gray-600 mobile:text-h-5-regular">
-          {recentSearchList?.map((keyword) => (
-            <li className="flex justify-between" key={keyword}>
-              {keyword}
-              <button onClick={() => handleStorage["itemClear"](keyword)}>
-                <Svg id="cancel" size={24} color={"A0A0A0"} />
-              </button>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </>
-  );
-}
-
-interface TSearchModal {
-  className?: string;
-  open: boolean;
-  closeFn: () => void;
-}
-
-export default function SearchModal({
-  closeFn,
-  className,
-  open,
-}: TSearchModal) {
   return (
     <dialog
-      className={`bg-none fixed inset-0 z-10 flex h-svh w-full flex-col items-end bg-opacity-50 p-[82px] font-sans mobile:p-4 ${className}`}
+      className={`bg-none fixed inset-0 z-10 flex h-svh w-full flex-col items-end bg-opacity-50 p-[82px] font-sans mobile:p-10 ${className}`}
       open={open}
     >
-      <button onClick={closeFn} className="pb-[53px]">
+      <button onClick={closeFn} className="pb-[53px] mobile:hidden">
         <Svg id="cancel" size={48} color={"A0A0A0"} />
       </button>
-      <RecentSearchKeyword />
+      <button onClick={closeFn} className="hidden pb-[53px] mobile:block">
+        <Svg id="cancel" size={28} color={"A0A0A0"} />
+      </button>
+      <div className="min-w-[50%] mobile:w-full">
+        <SearchInput setList={setRecentSearchList} />
+        <RecentSearchKeyword
+          strageFn={handleStorage}
+          recentSearchList={recentSearchList}
+        />
+      </div>
     </dialog>
   );
 }
--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -4,3 +4,4 @@ export { default as SearchModal } from "./SearchModal";
 export { default as MobileHeader } from "./MobileHeader";
 export { default as DesktopHeader } from "./DesktopHeader";
 export { default as MobileHamburger } from "./MobileHamburger";
+export { default as RecentSearchKeyword } from "./RecentSearchKeyword";
