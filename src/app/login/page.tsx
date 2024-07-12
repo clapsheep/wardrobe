374cb96e99c2ea633eb9a9fb56582e1dd19a2b57
clapsheep <sktndid1203@naver.com>
2024-07-12 16:33:01 +0900

    [feat] sign in 마크업 #35 - BasicInput 컴포넌트 제작 - svg 비밀번호 표시 추가 - 로그인 마크업

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,40 @@
+import { BasicButton, BasicInput } from "@/components/atoms";
+
 const LoginPage = () => {
-  return <div>login 페이지 입니다.</div>;
+  return (
+    <main className="mx-auto my-0 flex w-[440px] flex-col items-center justify-center py-[50px]">
+      <h1 className="inline-block w-full border-b-4 border-black pb-4 text-center text-h-1-regular">
+        로그인
+      </h1>
+      <form className="w-full">
+        <BasicInput
+          type="text"
+          // label="아이디"
+          placeholder="아이디(이메일)을 입력하세요."
+          className="my-4 h-12"
+          id="userId"
+        />
+        <BasicInput
+          type="password"
+          // label="비밀번호"
+          placeholder="비밀번호를 입력하세요."
+          className="my-4 h-12"
+          id="password"
+        />
+        <BasicButton type="submit" size="lg" color="primary">
+          로그인 하기
+        </BasicButton>
+      </form>
+      <BasicButton
+        className="mt-4 rounded-full"
+        size="lg"
+        color="secondary"
+        href="/"
+      >
+        간편 회원가입하기
+      </BasicButton>
+    </main>
+  );
 };
 
 export default LoginPage;
---
+++ src/components/atoms/BasicInput.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import React, { useState } from "react";
+import { Svg } from "@/components/atoms";
+
+interface Tinput {
+  id: string;
+  type: "text" | "password";
+  placeholder: string;
+  label?: string;
+  className?: string;
+}
+
+const BasicInput = ({ id, type, placeholder, label, className }: Tinput) => {
+  const [inputTypeState, setInputTypeState] = useState(type);
+
+  const handleVisibility = () => {
+    setInputTypeState((prevType) =>
+      prevType === "password" ? "text" : "password",
+    );
+  };
+
+  const visibilityLabel = {
+    true: "비밀번호가 화면에 문자로 보여지도록 합니다.",
+    false: "비밀번호가 화면에서 보여지지 않도록 합니다.",
+  };
+
+  return (
+    <div
+      className={`relative flex w-full items-center justify-center ${className}`}
+    >
+      <label
+        className={`${label ? "" : "sr-only"} w-[68px] text-b-4-semibold`}
+        htmlFor={id}
+      >
+        {label ?? placeholder}
+      </label>
+      <input
+        id={id}
+        type={inputTypeState}
+        className="h-full w-full rounded-sm border border-gray-300 px-[14px] py-[10px] text-b-2-regular placeholder:text-gray-300"
+        placeholder={placeholder}
+      />
+      {type !== "password" ? (
+        ""
+      ) : (
+        <button
+          className="absolute right-3"
+          aria-label={
+            inputTypeState === "password"
+              ? visibilityLabel.false
+              : visibilityLabel.true
+          }
+          type="button"
+          onClick={handleVisibility}
+        >
+          <Svg
+            size={28}
+            id={
+              inputTypeState === "password" ? "visibility_on" : "visibility_off"
+            }
+          />
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default BasicInput;
