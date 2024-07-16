80de34985cc10626e5f48352bb1c1c3788057abe
jio <pennicillium@daum.net>
2024-07-16 23:15:32 +0900

    [feat] Progressbar 컴포넌트 생성 #36

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -5,3 +5,4 @@ export { default as TagButton } from "./TagButton";
 export { default as BasicInput } from "./BasicInput";
 export { default as BasicButton } from "./BasicButton";
 export { default as SearchInput } from "./SearchInput";
+export {default as Progressbar } from "./Progressbar"
