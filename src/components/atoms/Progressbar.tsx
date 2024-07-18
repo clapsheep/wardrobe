cbaeaa320e064d583e9366b44bc1e452eef95d26
jio <pennicillium@daum.net>
2024-07-18 22:42:33 +0900

    [modify] progressbar 표시 안 되는 문제 해결 #36

--- src/components/atoms/Progressbar.tsx
+++ src/components/atoms/Progressbar.tsx
@@ -3,10 +3,9 @@ interface TProgressbar {
   step: number;
 }
 const Progressbar = ({ max, step }: TProgressbar) => {
-  const rate = (step / max) * 100;
   const style = {
     bar: `flex h-1.5 w-full overflow-hidden bg-gray-100`,
-    progress: `flex flex-col will-change justify-center overflow-hidden whitespace-nowrap bg-black text-center text-xs text-white transition duration-500 ease-in-out w-[${rate}%]`,
+    progress: `flex flex-col will-change justify-center overflow-hidden whitespace-nowrap bg-black text-center text-xs text-white transition duration-500 ease-in-out w-[${(step / max) * 100}%]`,
   };
   return (
     <div className={`${style.bar}`} role="progressbar">
