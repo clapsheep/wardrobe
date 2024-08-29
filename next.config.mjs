9fa0790bde1a7804b21527f14589c9d587ac6883
clapsheep <sktndid1203@naver.com>
2024-08-29 18:10:06 +0900

    [feat] public 캐싱 설정

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,17 @@ const nextConfig = {
   images: {
     domains: ["image.msscdn.net", "img.29cm.co.kr"], // 추가된 부분
   },
+  headers: async () => [
+    {
+      source: "/images/(.*)",
+      headers: [
+        {
+          key: "Cache-Control",
+          value: "public, max-age=1000, immutable",
+        },
+      ],
+    },
+  ],
 };
 
 export default nextConfig;
